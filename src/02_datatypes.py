2c3f3eae763887ed3a0f1cb53c515fe92d2df741
Genard Ivan Tejano <genardivanteajno@Genards-MacBook-Pro.local>
2020-07-06 12:52:44 -0700

    exercises 00-02 completed

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,13 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
+y = 7
+print( x + y )
 
 
 # Write a print statement that combines x + y into the string value 57
+x = "5"
+y = "7"
+print( x + y )
 
 # YOUR CODE HERE
